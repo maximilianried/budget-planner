6d6028af4508f1cf2adb21f11f0e88d9adc4e630
maximilianried <79225941+maximilianried@users.noreply.github.com>
2022-01-13 21:02:57 +0100

    Added statistics

--- src/components/statistics/Statistic.js
+++ src/components/statistics/Statistic.js
@@ -1,8 +1,23 @@
 import React from "react";
-import { Button, Container, Stack } from "react-bootstrap";
-import StatisticCard from "./StatisticCard";
+import { Button, Container, Stack, Card } from "react-bootstrap";
+import { useBudgets } from "../../contexts/BudgetsContext";
+import { Bar, Doughnut, Bubble } from "react-chartjs-2";
+import Chart from "chart.js/auto";
 
 export default function Statistic() {
+  const { budgets, getBudgetExpenses } = useBudgets();
+
+  var labelArray = budgets.map((budget) => {
+    return budget.name;
+  });
+
+  var amountArray = budgets.map((budget) => {
+    return getBudgetExpenses(budget.id).reduce(
+      (total, expense) => total + expense.amount,
+      0
+    );
+  });
+
   return (
     <>
       <Container className="my-4">
@@ -23,10 +38,65 @@ export default function Statistic() {
             alignItems: "flex-start",
           }}
         >
-          <StatisticCard />
-          <StatisticCard />
-          <StatisticCard />
-          <StatisticCard />
+          <Card>
+            <Card.Body>
+              <Card.Title>Spendings</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Spendings per Budget
+              </Card.Subtitle>
+              <Card.Text>
+                <Bar
+                  data={{
+                    labels: labelArray,
+                    datasets: [
+                      {
+                        label: "Budgets",
+                        data: amountArray,
+                        backgroundColor: [
+                          "rgb(92, 184, 92)",
+                          "rgb(91, 192, 222)",
+                          "rgb(240, 173, 78)",
+                          "rgba(255, 99, 132)",
+                          "rgba(54, 162, 235)",
+                          "rgba(255, 206, 86)",
+                        ],
+                      },
+                    ],
+                  }}
+                />
+              </Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Card>
+            <Card.Body>
+              <Card.Title>Combined</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                All Budgets combined
+              </Card.Subtitle>
+              <Card.Text>
+                <Doughnut
+                  data={{
+                    labels: labelArray,
+                    datasets: [
+                      {
+                        label: "Budgets",
+                        data: amountArray,
+                        backgroundColor: [
+                          "rgb(92, 184, 92)",
+                          "rgb(91, 192, 222)",
+                          "rgb(240, 173, 78)",
+                          "rgba(255, 99, 132)",
+                          "rgba(54, 162, 235)",
+                          "rgba(255, 206, 86)",
+                        ],
+                      },
+                    ],
+                  }}
+                />
+              </Card.Text>
+            </Card.Body>
+          </Card>
         </div>
       </Container>
     </>
