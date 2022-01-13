371e72bac25fec6699d78a265ed2e863a4df01ce
maximilianried <79225941+maximilianried@users.noreply.github.com>
2022-01-13 14:38:25 +0100

    Build the 404 Page

--- src/components/NoPage.js
+++ src/components/NoPage.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Container, Stack, Button } from "react-bootstrap";
 
 export default function NoPage() {
   return (
-    <div>
-      <h1>404</h1>
-    </div>
+    <Container className="my-4">
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h2 className="me-auto" style={{ fontWeight: "bold" }}>
+          404
+        </h2>
+        <Button variant="primary" href="/budget">
+          <i className="bi bi-house" style={{ marginRight: "6px" }} />
+          Back Home
+        </Button>
+      </Stack>
+      <div>Error 404 Page not found</div>
+    </Container>
   );
 }
--- src/components/settings/Setting.js
+++ src/components/settings/Setting.js
@@ -1,5 +1,26 @@
 import React from "react";
+import { Button, Container, Stack } from "react-bootstrap";
 
 export default function Setting() {
-  return <div>Settings</div>;
+  return (
+    <Container className="my-4">
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h2 className="me-auto" style={{ fontWeight: "bold" }}>
+          Statistics
+        </h2>
+        <Button variant="primary" onClick={() => window.location.reload()}>
+          <i className="bi bi-arrow-repeat" style={{ marginRight: "6px" }} />
+          Recompute
+        </Button>
+      </Stack>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(500px, 1fr))",
+          gap: "1rem",
+          alignItems: "flex-start",
+        }}
+      ></div>
+    </Container>
+  );
 }
